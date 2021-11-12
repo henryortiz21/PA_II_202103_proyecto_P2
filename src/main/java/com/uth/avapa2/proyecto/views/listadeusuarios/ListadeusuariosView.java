93b2e0a69c9abf64ac830fcd4c9ecc692b9c6a67
henryortiz21 <jerryortiz2100@gmail.com>
2021-11-11 21:01:02 -0600

    Lista de usuarios de acuerdo a los creados...

--- src/main/java/com/uth/avapa2/proyecto/views/listadeusuarios/ListadeusuariosView.java
+++ src/main/java/com/uth/avapa2/proyecto/views/listadeusuarios/ListadeusuariosView.java
@@ -1,170 +1,191 @@
 package com.uth.avapa2.proyecto.views.listadeusuarios;
+import java.util.Optional;
 
-import java.awt.*;
-import java.util.Arrays;
-import java.util.List;
-
+import com.uth.avapa2.proyecto.data.entity.Usuario;
+import com.uth.avapa2.proyecto.data.service.UsuarioService;
+import com.uth.avapa2.proyecto.views.MainLayout;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.artur.helpers.CrudServiceDataProvider;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
-import com.uth.avapa2.proyecto.views.MainLayout;
+import com.vaadin.flow.component.textfield.TextField;
 
 @PageTitle("Lista de usuarios")
-@Route(value = "listaUsuarios", layout = MainLayout.class)
-public class ListadeusuariosView extends Div implements AfterNavigationObserver {
+@Route(value = "listausuarios/:usuarioID?/:action?(edit)", layout = MainLayout.class)
+public class ListadeusuariosView extends Div implements BeforeEnterObserver {
 
-    private Button bNuevoUsuario=new Button("Nuevo usuario");
-    Grid<Person> grid = new Grid<>();
+    private final String USUARIO_ID = "usuarioID";
+    private final String USUARIO_EDIT_ROUTE_TEMPLATE = "listausuarios/%d/edit";
 
-    public ListadeusuariosView() {
-        addClassName("listadeusuarios-view");
-        setSizeFull();
-        grid.setHeight("100%");
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
-        grid.addComponentColumn(person -> createCard(person));
-        add(createButtonLayout());
-        add(grid);
-    }
-    private Component createButtonLayout() {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.addClassName("button-layout");
-        bNuevoUsuario.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(bNuevoUsuario);
-        return buttonLayout;
-    }
-    private HorizontalLayout createCard(Person person) {
-        HorizontalLayout card = new HorizontalLayout();
-        card.addClassName("card");
-        card.setSpacing(false);
-        card.getThemeList().add("spacing-s");
+    private Grid<Usuario> grid = new Grid<>(Usuario.class, false);
 
-        Image image = new Image();
-        image.setSrc(person.getImage());
-        VerticalLayout description = new VerticalLayout();
-        description.addClassName("description");
-        description.setSpacing(false);
-        description.setPadding(false);
+    private TextField nombre;
+    private TextField apellido;
+    private TextField telefono;
+    private TextField email;
 
-        HorizontalLayout header = new HorizontalLayout();
-        header.addClassName("header");
-        header.setSpacing(false);
-        header.getThemeList().add("spacing-s");
+    private Button bCancelar = new Button("Cancelar");
+    private Button bGuardar = new Button("Guardar");
 
-        Span name = new Span(person.getName());
-        name.addClassName("name");
-        Span date = new Span(person.getDate());
-        date.addClassName("date");
-        header.add(name, date);
+    private BeanValidationBinder<Usuario> binder;
 
-        Span post = new Span(person.getPost());
-        post.addClassName("post");
+    private Usuario usuario;
 
-        HorizontalLayout actions = new HorizontalLayout();
-        actions.addClassName("actions");
-        actions.setSpacing(false);
-        actions.getThemeList().add("spacing-s");
+    private UsuarioService usuarioService;
 
-        Icon likeIcon = VaadinIcon.HEART.create();
-        likeIcon.addClassName("icon");
-        Span likes = new Span(person.getLikes());
-        likes.addClassName("likes");
-        Icon commentIcon = VaadinIcon.COMMENT.create();
-        commentIcon.addClassName("icon");
-        Span comments = new Span(person.getComments());
-        comments.addClassName("comments");
-        Icon shareIcon = VaadinIcon.CONNECT.create();
-        shareIcon.addClassName("icon");
-        Span shares = new Span(person.getShares());
-        shares.addClassName("shares");
+    public ListadeusuariosView(@Autowired UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+        addClassNames("listadeusuarios-view", "flex", "flex-col", "h-full");
+        // Create UI
+        SplitLayout splitLayout = new SplitLayout();
+        splitLayout.setSizeFull();
 
-        actions.add(likeIcon, likes, commentIcon, comments, shareIcon, shares);
+        createGridLayout(splitLayout);
+        createEditorLayout(splitLayout);
 
-        description.add(header, post, actions);
-        card.add(image, description);
-        return card;
+        add(splitLayout);
+
+        // Configure Grid
+        grid.addColumn("nombre").setAutoWidth(true);
+        grid.addColumn("apellido").setAutoWidth(true);
+        grid.addColumn("telefono").setAutoWidth(true);
+        grid.addColumn("email").setAutoWidth(true);
+        grid.setDataProvider(new CrudServiceDataProvider<>(usuarioService));
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.setHeightFull();
+
+        // when a row is selected or deselected, populate form
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            if (event.getValue() != null) {
+                UI.getCurrent().navigate(String.format(USUARIO_EDIT_ROUTE_TEMPLATE, event.getValue().getId()));
+            } else {
+                clearForm();
+                UI.getCurrent().navigate(ListadeusuariosView.class);
+            }
+        });
+
+        // Configure Form
+        binder = new BeanValidationBinder<>(Usuario.class);
+
+        // Bind fields. This where you'd define e.g. validation rules
+
+        binder.bindInstanceFields(this);
+
+        bCancelar.addClickListener(e -> {
+            clearForm();
+            refreshGrid();
+        });
+
+        bGuardar.addClickListener(e -> {
+            try {
+                if (this.usuario == null) {
+                    this.usuario = new Usuario();
+                }
+                binder.writeBean(this.usuario);
+
+                usuarioService.update(this.usuario);
+                clearForm();
+                refreshGrid();
+                Notification.show("Usuario details stored.");
+                UI.getCurrent().navigate(ListadeusuariosView.class);
+            } catch (ValidationException validationException) {
+                Notification.show("An exception happened while trying to store the usuario details.");
+            }
+        });
+
     }
 
     @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-
-        // Set some data when this view is displayed.
-        List<Person> persons = Arrays.asList( //
-                createPerson("https://randomuser.me/api/portraits/men/42.jpg", "John Smith", "May 8",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/42.jpg", "Abagail Libbie", "May 3",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/24.jpg", "Alberto Raya", "May 3",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/24.jpg", "Emmy Elsner", "Apr 22",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/76.jpg", "Alf Huncoot", "Apr 21",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/76.jpg", "Lidmila Vilensky", "Apr 17",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/94.jpg", "Jarrett Cawsey", "Apr 17",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/94.jpg", "Tania Perfilyeva", "Mar 8",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/16.jpg", "Ivan Polo", "Mar 5",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/16.jpg", "Emelda Scandroot", "Mar 5",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/67.jpg", "Marcos Sá", "Mar 4",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/67.jpg", "Jacqueline Asong", "Mar 2",
-
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20")
-
-        );
-
-        grid.setItems(persons);
+    public void beforeEnter(BeforeEnterEvent event) {
+        Optional<Integer> usuarioId = event.getRouteParameters().getInteger(USUARIO_ID);
+        if (usuarioId.isPresent()) {
+            Optional<Usuario> usuarioFromBackend = usuarioService.get(usuarioId.get());
+            if (usuarioFromBackend.isPresent()) {
+                populateForm(usuarioFromBackend.get());
+            } else {
+                Notification.show(String.format("The requested usuario was not found, ID = %d", usuarioId.get()), 3000,
+                        Notification.Position.BOTTOM_START);
+                // when a row is selected but the data is no longer available,
+                // refresh grid
+                refreshGrid();
+                event.forwardTo(ListadeusuariosView.class);
+            }
+        }
     }
 
-    private static Person createPerson(String image, String name, String date, String post, String likes,
-            String comments, String shares) {
-        Person p = new Person();
-        p.setImage(image);
-        p.setName(name);
-        p.setDate(date);
-        p.setPost(post);
-        p.setLikes(likes);
-        p.setComments(comments);
-        p.setShares(shares);
+    private void createEditorLayout(SplitLayout splitLayout) {
+        Div editorLayoutDiv = new Div();
+        editorLayoutDiv.setClassName("flex flex-col");
+        editorLayoutDiv.setWidth("400px");
 
-        return p;
+        Div editorDiv = new Div();
+        editorDiv.setClassName("p-l flex-grow");
+        editorLayoutDiv.add(editorDiv);
+
+        FormLayout formLayout = new FormLayout();
+        nombre = new TextField("Nombre");
+        apellido = new TextField("Apellido");
+        telefono = new TextField("Telefono");
+        email = new TextField("Email");
+        Component[] fields = new Component[]{nombre, apellido, telefono, email};
+
+        for (Component field : fields) {
+            ((HasStyle) field).addClassName("full-width");
+        }
+        formLayout.add(fields);
+        editorDiv.add(formLayout);
+        createButtonLayout(editorLayoutDiv);
+
+        splitLayout.addToSecondary(editorLayoutDiv);
     }
 
+    private void createButtonLayout(Div editorLayoutDiv) {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.setClassName("w-full flex-wrap bg-contrast-5 py-s px-l");
+        buttonLayout.setSpacing(true);
+        bCancelar.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        bGuardar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonLayout.add(bGuardar, bCancelar);
+        editorLayoutDiv.add(buttonLayout);
+    }
+
+    private void createGridLayout(SplitLayout splitLayout) {
+        Div wrapper = new Div();
+        wrapper.setId("grid-wrapper");
+        wrapper.setWidthFull();
+        splitLayout.addToPrimary(wrapper);
+        wrapper.add(grid);
+    }
+
+    private void refreshGrid() {
+        grid.select(null);
+        grid.getDataProvider().refreshAll();
+    }
+
+    private void clearForm() {
+        populateForm(null);
+    }
+
+    private void populateForm(Usuario value) {
+        this.usuario = value;
+        binder.readBean(this.usuario);
+
+    }
 }
